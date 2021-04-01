f8d9a11000f6c9f176135b654eadafadf11a9a5c
mpsnets <mpsnets@gmail.com>
2021-04-01 19:35:53 -0300

    Working with four verbs

---
+++ 02_RestWithAPSNET5_Calculadora/Calculadora/Calculadora/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public PersonController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
